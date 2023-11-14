e39dafdae899747178e3f8d866c77022fcab213e
PrimalTea47 <leandre.fouret@student-oec.fr>
2023-11-14 18:59:15 +0100

    removed back to menu button

--- level_selection.py
+++ level_selection.py
@@ -16,14 +16,14 @@ def lauching_level_selection_screen():
 	easy_button = pygame.Rect(330,140,135,70)
 	medium_button = pygame.Rect(280,320,220,70)
 	hard_button = pygame.Rect(330,500,135,70)
-	back_menu = pygame.Rect(220,720,360,70)
+	
 
 
 	#Variable d'activation de levels
 	easy_condition = False
 	medium_condition = False
 	hard_condition = False
-	back_to_menu_condition = False
+
 
 
 	#gestion d'évènements
@@ -35,11 +35,9 @@ def lauching_level_selection_screen():
 		pygame.draw.rect(screen, (63,153,38),easy_button)
 		pygame.draw.rect(screen, (224,127,15),medium_button)
 		pygame.draw.rect(screen, (193,24,15),hard_button)
-		pygame.draw.rect(screen, (146,136,155), back_menu)
 		write_text('Easy', font, (0,0,0), 340,150,screen)
 		write_text('Medium', font, (0,0,0), 290,330,screen)
 		write_text('Hard', font, (0,0,0), 340,510,screen)
-		write_text('Back to Menu', font, (255,255,255), 230,730, screen)
 
 		#vérification des lancements des levels
 		if easy_condition:
@@ -52,9 +50,7 @@ def lauching_level_selection_screen():
 			hard_level()
 
 
-		if back_to_menu_condition:
-			print('Back to menu')
-			back_to_menu_condition = False
+
 
 
 
@@ -74,8 +70,7 @@ def lauching_level_selection_screen():
 				if hard_button.collidepoint(event.pos):
 					hard_condition = True
 
-				if back_menu.collidepoint(event.pos):
-					back_to_menu_condition = True
+
 
 
 		pygame.display.flip()
